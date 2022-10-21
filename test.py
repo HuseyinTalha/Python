189b7d88803c8e1babcf04f678366144bd12b1d4
HuseyinTalha <92941904+HuseyinTalha@users.noreply.github.com>
2022-10-21 15:19:54 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import re
+'''import re
 
 while (True):
     a_string = input("gir")
@@ -15,7 +15,31 @@ while (True):
 
 
     else:
-        False
+        False'''
+
+import re
+while True:
+    a_string = input("gir")
+    sonuc = []
+
+
+    if not (re.findall(r'[a-zA-Z]', a_string)):
+        a_list = [x for x in a_string]
+        mapobject = map(int, a_list)
+        intlist = list(mapobject)
+        for i in range(len(intlist)):
+            if intlist[i] % 2 != 0:
+                if intlist[i+1] % 2 != 0:
+                    sonuc.append(intlist[i])
+                    sonuc.append("-")
+                    sonuc.append(intlist[i+1])
+
+
+            elif intlist[i] % 2 ==0 or intlist[i+1] % 2 !=0:
+                sonuc.append(intlist[i])
+                sonuc.append(intlist[i+1])
+            print(sonuc)
+
 
 
 
